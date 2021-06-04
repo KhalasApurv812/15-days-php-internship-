7310630e09157bfccbc0a94a4d01f28493a26e35
Apurv <85150669+KhalasApurv812@users.noreply.github.com>
2021-06-04 10:51:21 +0530

    Add files via upload

---
+++ process.php
@@ -0,0 +1,9 @@
+<?php
+ 
+$a= $_GET['txt1'];
+$b= $_GET['txt2'];
+$c= $_GET['txt3'];
+$d= $_GET['txt4'];
+$e= $_GET['txt5'];
+
+echo "hi my name is $a and<br> monbile no is $b and<br> age is $c and <br>email id is $d and <br>passwod is $e";

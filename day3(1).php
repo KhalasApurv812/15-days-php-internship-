b3e582d4cd37a1552c92e2bd5fdbd49a3aeb5ff2
Apurv <85150669+KhalasApurv812@users.noreply.github.com>
2021-06-04 10:59:24 +0530

    Add files via upload
    using post method

---
+++ day3(1).php
@@ -0,0 +1,41 @@
+<html>
+    
+    <body>
+        <h1>Registration Form</h1>
+        <form action="process.php" method="GET">
+             <table>
+                 <tr>
+                     <td>Name:</td>
+                     <td><input type="text" name="te"\></td>
+                 </tr>
+                 
+                 <tr>
+                     <td>Mobile NO:</td>
+                     <td><input type="number" name="txt2"\></td>
+                 </tr>
+                 
+                 <tr> 
+                     <td>Age:</td>
+                     <td><input type="number" name="txt3"\></td>
+                 </tr>
+                 
+                 <tr>
+                     <td>Email id:</td>
+                     <td><input type="text" name="txt4"\></td>
+                 </tr>
+                 
+                 <tr>
+                     <td>Password:</td>
+                     <td><input type="password" name="txt5"\></td>
+                 </tr>
+                 <tr>
+                     <td><input type="submit"/></td>
+                     <td><input type="Reset"/></td>
+                     
+                 </tr>
+             </table>
+
+       </form>
+</body>
+</html>
+   
---
+++ process1.php
@@ -0,0 +1,9 @@
+<?php
+ 
+$a= $_POST['txt1'];
+$b= $_POST['txt2'];
+$c= $_POST['txt3'];
+$d= $_POST['txt4'];
+$e= $_POST['txt5'];
+
+echo "hi my name is $a and<br> monbile no is $b and<br> age is $c and <br>email id is $d and <br>passwod is $e";

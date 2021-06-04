3431dde436698dc317e6e872358218278a3397d7
Apurv <85150669+KhalasApurv812@users.noreply.github.com>
2021-06-04 20:58:10 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>Apurv Khalas</title>
+<link href="http://fonts.googleapis.com/css?family=Arvo" rel="stylesheet" type="text/css" />
+<link href="http://fonts.googleapis.com/css?family=Coda:400,800" rel="stylesheet" type="text/css" />
+<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+<body>
+<?php
+ include './thempart/menu.php';
+ include './thempart/logo.php';
+?>
+
+<div id="wrapper"> 
+	<!-- end #header -->
+	<div id="page-bgtop"></div>
+	<div id="page">
+            <div><img src="slider.jpg.jpg" width="920" height="450" alt="" /></div>
+		<div id="content">
+			<div class="post">
+				<h2 class="title"><a href="#">Wlecome to my website</a></h2>
+				<div style="clear: both;">&nbsp;</div>
+				<div class="entry">
+                                    Hello, MY Name is Khalas Apurv.
+					
+                                </div>
+			</div>
+			
+			<div style="clear: both;">&nbsp;</div>
+		</div>
+		<!-- end #content -->
+		<?php
+                include './thempart/sidebar.php';
+                ?>
+		<!-- end #sidebar -->
+		<div style="clear: both;">&nbsp;</div>
+	</div>
+	<div id="page-bgbtm"></div>
+	<!-- end #page --> 
+</div>
+<?php
+include './thempart/footer.php';
+?>
+<!-- end #footer -->
+</body>
+</html>
+
+
